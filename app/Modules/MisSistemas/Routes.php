ea633eb4f3f378fbbd1e45be8a2b1e45e95413ff
JoanaHdez <ciahdez22@gmail.com>
2026-08-24 18:05:18 -0600

    v-240826-1805

--- app/Modules/MisSistemas/Routes.php
+++ app/Modules/MisSistemas/Routes.php
@@ -5,13 +5,15 @@ use App\Modules\MisSistemas\Controllers\MisSistemas_Controller;
 
 /** @var RouteCollection $routes */
 
+
+/*==================================================
+=               MIS SISTEMAS PÚBLICO               =
+==================================================*/
+
 $routes->get(
     'mis-sistemas',
     [
         MisSistemas_Controller::class,
         'index',
-    ],
-    [
-        'filter' => 'auth',
     ]
 );
